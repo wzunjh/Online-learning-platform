b7067bf94f9658465350a37bc733c673f80c5773
njh <wzunjh@qq.com>
2023-02-06 16:28:10 +0800

    接口的测试

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/content/api/CourseBaseInfoController.java
@@ -4,11 +4,14 @@ import com.xuecheng.content.base.model.PageParams;
 import com.xuecheng.content.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @description TODO
@@ -20,10 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseBaseInfoController {
 
+    @Resource
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams params, @RequestBody QueryCourseParamsDto queryCourseParamsDto){
-     return  null;
+
+        return courseBaseInfoService.queryCourseBaseList(params, queryCourseParamsDto);
     }
 
 }
